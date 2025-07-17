3e2c399e572257f585b09648ec7a70ae8f06079c
JunSeoChoi <evan7484@gmail.com>
2025-07-17 15:56:04 +0900

    Modify: 와인선택화면 수정

--- src/pages/MainPage.tsx
+++ src/pages/MainPage.tsx
@@ -1,12 +1,20 @@
 import styles from "../styles/MainPage.module.css";
+import Wine_1 from "../assets/Wine_1.jpg";
+import Wine_2 from "../assets/Wine_2.jpg";
 
 const MainPage = () => {
   return (
     <>
       <div className={styles.header}>🍷WineQueen🍷</div>
       <div className={styles.section}>
-        <div className={styles.rectangle}>카메라</div>
-        <div className={styles.sensor}>압력센서</div>
+        <div className={styles.rectangle}>
+          <img src={Wine_1} alt="와인1" />
+          <div>❶</div>
+        </div>
+        <div className={styles.sensor}>
+          <img src={Wine_2} alt="와인2" />
+          <div>❷</div>
+        </div>
       </div>
     </>
   );
