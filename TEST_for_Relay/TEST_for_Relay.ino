f2e93e8b5214d5acaeb145b055495968d1f9cc30
kywls405 <kimyongjin345@gmail.com>
2025-07-17 14:23:35 +0900

    Initial commit

---
+++ TEST_for_Relay/TEST_for_Relay.ino
@@ -0,0 +1,19 @@
+int RelayPin = 8;
+
+void setup() {
+  Serial.begin(9600);
+  pinMode(RelayPin,OUTPUT);
+  
+  // put your setup code here, to run once:
+
+}
+
+void loop() {
+  // put your main code here, to run repeatedly:
+//   digitalWrite(RelayPin,LOW);
+//   Serial.println("HIGH");
+//   delay(5000);
+//   digitalWrite(RelayPin,HIGH);
+//   Serial.println("LOW");
+//   delay(2000);
+}
